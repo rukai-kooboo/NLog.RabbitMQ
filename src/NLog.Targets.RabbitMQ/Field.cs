6aa9fa4f8d766c10019a5dd9429cd8b06647a785
ErikRydgren <erik@rydgrens.net>
2014-03-11 17:45:27 +0100

    Support json output fields defined in NLog configuration
    Support json fields defined as Layout in NLog config.
    
    <target xsi:type="RabbitMQ" useJSON="true" ...>
    <field name="threadid" layout="${threadid}"/>
    <field name="instance" layout="Node-12"/>
    </target>
    
    Configuration defined fields are overridden if same field occurr as
    LogEvent property.

---
+++ src/NLog.Targets.RabbitMQ/Field.cs
@@ -0,0 +1,45 @@
+﻿namespace NLog.Targets
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using NLog.Config;
+	using NLog.Layouts;
+
+	[NLogConfigurationItem]
+	[ThreadAgnostic]
+	public class Field
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Field" /> class.
+		/// </summary>
+		public Field()
+			: this(null, null)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Field" /> class.
+		/// </summary>
+		/// <param name="name">The field name</param>
+		/// <param name="layout">The field layout</param>
+		public Field(string name, Layout layout)
+		{
+			this.Name = name;
+			this.Layout = layout;
+		}
+
+		/// <summary>
+		/// Gets or sets the name of the field
+		/// </summary>
+		[RequiredParameter]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the layout of the field
+		/// </summary>
+		[RequiredParameter]
+		public Layout Layout { get; set; }
+	}
+}
